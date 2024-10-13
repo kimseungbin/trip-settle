30e9340710994d584b430ed880b5d20fdeaa0e2d
SeungBin Kim <luxorienskim@gmail.com>
2024-10-13 20:06:45 +0900

    Refactor: Use TripsRepository for trip lookup
    Updated TripsService to use an injected TripsRepository for finding trips. Modified the find method to be asynchronous and return the trip details from the repository. This improves the service's data lookup handling and aligns with dependency injection practices.

--- src/trips/trips.service.ts
+++ src/trips/trips.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { CreateTripDto } from './dto/create-trip.dto'
 import { UpdateTripDto } from './dto/update-trip.dto'
 
 @Injectable()
 export class TripsService {
+	constructor(@Inject('TripsRepository') private readonly tripsRepository: any) {}
+
 	create(createTripDto: CreateTripDto) {
 		return 'This action adds a new trip'
 	}
 
-	find(id: string) {
-		return {}
+	async find(id: string) {
+		const trip = await this.tripsRepository.findOne(id)
+
+		return trip
 	}
 
 	update(id: number, updateTripDto: UpdateTripDto) {
