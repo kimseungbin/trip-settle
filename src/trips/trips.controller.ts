fee3741b7c0fdc0c0f9bd515f5853d449da46cab
SeungBin Kim <luxorienskim@gmail.com>
2024-10-13 12:46:57 +0900

    Update: Trip creation response handling
    Improved the create trip endpoint to set status and location headers. This change includes using the express Response object to return a 201 status and location for the new trip.

--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common'
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, Res } from '@nestjs/common'
 import { TripsService } from './trips.service'
 import { CreateTripDto } from './dto/create-trip.dto'
 import { UpdateTripDto } from './dto/update-trip.dto'
 import { FindTripDto } from './dto/find-trip.dto'
+import { Response } from 'express'
 
 @Controller('trips')
 export class TripsController {
 	constructor(private readonly tripsService: TripsService) {}
 
 	@Post()
-	create(@Body() createTripDto: CreateTripDto) {
-		return this.tripsService.create(createTripDto)
+	async create(@Body() createTripDto: CreateTripDto, @Res() res: Response): Promise<void> {
+		const trip = await this.tripsService.create(createTripDto)
+		res.status(201).location(`/trips/${trip.id}`).send()
 	}
 
 	@Get(':id')
