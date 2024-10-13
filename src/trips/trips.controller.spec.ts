cd4856ac43b419888020cf4078c2325701801192
SeungBin Kim <luxorienskim@gmail.com>
2024-10-13 10:23:16 +0900

    Test: Mock TripsService in TripsController tests
    Replaced actual TripsService with a mock version in TripsController tests to isolate and verify the controller’s behavior. Added a new test to ensure the find method returns an array of trips and the TripsService's findAll method is called.

--- src/trips/trips.controller.spec.ts
+++ src/trips/trips.controller.spec.ts
@@ -2,19 +2,37 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { TripsController } from './trips.controller'
 import { TripsService } from './trips.service'
 
+const mockTripsService = {
+	find: jest.fn().mockReturnValue({}),
+}
+
 describe('TripsController', () => {
 	let controller: TripsController
+	let service: TripsService
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [TripsController],
-			providers: [TripsService],
+			providers: [
+				{
+					provide: TripsService,
+					useValue: mockTripsService,
+				},
+			],
 		}).compile()
 
 		controller = module.get<TripsController>(TripsController)
+		service = module.get<TripsService>(TripsService)
 	})
 
 	it('should be defined', () => {
 		expect(controller).toBeDefined()
+	})
+
+	describe('find', () => {
+		it('should return a trip', async () => {
+			expect(await controller.find()).toEqual({})
+			expect(service.find).toHaveBeenCalled()
+		})
 	})
 })
