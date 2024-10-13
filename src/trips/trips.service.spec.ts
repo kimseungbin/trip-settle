164184c7e1484cdb619790bd8b7bcef504f59766
SeungBin Kim <luxorienskim@gmail.com>
2024-10-13 19:39:36 +0900

    Test: Add tests for TripsService methods
    Introduced tests for the TripsService methods including find, update, and delete. Additionally, added a unique identifier generation for each test instance using randomBytes.

--- src/trips/trips.service.spec.ts
+++ src/trips/trips.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { TripsService } from './trips.service'
+import { randomBytes } from 'crypto'
 
 describe('TripsService', () => {
 	let service: TripsService
+	let base64TripId: string
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			providers: [TripsService],
 		}).compile()
 
+		jest.clearAllMocks()
+
 		service = module.get<TripsService>(TripsService)
+		base64TripId = randomBytes(16).toString('base64url')
 	})
 
 	it('should be defined', () => {
 		expect(service).toBeDefined()
 	})
+
+	describe('find', () => {
+		it('should return the trip object', async () => {
+			const expectedTrip = {}
+
+			const trip = await service.find(base64TripId)
+			expect(trip).toEqual(expectedTrip)
+		})
+	})
+	describe('update', () => {})
+	describe('delete', () => {})
 })
