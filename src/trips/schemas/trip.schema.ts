502e2bc0967c95447e269f4d80646208f88896d7
SeungBin Kim <luxorienskim@gmail.com>
2024-10-14 21:50:46 +0900

    Refactor: Update TripDocument to use HydratedDocument
    Replaced Trip & Document with HydratedDocument<Trip> to improve type accuracy and maintainability. This change leverages the benefits of mongoose's HydratedDocument type.

--- src/trips/schemas/trip.schema.ts
+++ src/trips/schemas/trip.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document, Types } from 'mongoose'
+import { Document, HydratedDocument, Types } from 'mongoose'
 
-export type TripDocument = Trip & Document
+export type TripDocument = HydratedDocument<Trip>
 
 @Schema()
 export class Trip {
