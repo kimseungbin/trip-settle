a1dcc7f1b6695a70992e25a095664f29dd2151c0
kimseungbin <luxorienskim@gmail.com>
2024-10-14 13:57:28 +0900

    Docs: Add JSDoc comments to FindTripDto properties
    Added JSDoc comments to provide clear documentation for the properties in the FindTripDto class, including 'id', 'participants', and 'expenses'. This enhances code readability and helps with auto-generated documentation.

--- src/trips/dto/find-trip.dto.ts
+++ src/trips/dto/find-trip.dto.ts
@@ -3,13 +3,22 @@ import { IsArray, IsString, ValidateNested } from 'class-validator'
 import { Type } from 'class-transformer'
 
 export class FindTripDto {
+	/**
+	 * Unique Base64URL format ID of the trip.
+	 */
 	@IsString()
 	id: string
 
+	/**
+	 * Array of all participants in the trip.
+	 */
 	@IsArray()
 	@IsString({ each: true })
 	participants: string[]
 
+	/**
+	 * Array of expenses associated with the trip.
+	 */
 	@IsArray()
 	@ValidateNested({ each: true })
 	@Type(() => ExpenseDto)
