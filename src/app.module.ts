102f1c9224f906b579eb055a2518fd7c012e6e78
kimseungbin <luxorienskim@gmail.com>
2024-10-14 15:53:04 +0900

    Feat: Integrate in-memory MongoDB for testing
    Implemented Mongoose with MongoMemoryServer to set up an in-memory MongoDB instance for testing environments.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TripsModule } from './trips/trips.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
 @Module({
-	imports: [TripsModule],
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: async () => {
+				const server = await MongoMemoryServer.create()
+				return {
+					uri: server.getUri(),
+				}
+			},
+		}),
+		TripsModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
