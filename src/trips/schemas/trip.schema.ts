cbdfd4f24898c01333d24b1a7b1a22094236223a
kimseungbin <luxorienskim@gmail.com>
2024-10-14 17:52:29 +0900

    Feat: Virtual `id` property to `Trip` schema
    This commit introduces a virtual property `id` to the `Trip` schema, which converts `_id` from ObjectId to a Base64URL string. This makes the schema more consistent and user-friendly when handling JSON objects. The `toJSON` and `toObject` transformations are updated to include virtual properties.

--- src/trips/schemas/trip.schema.ts
+++ src/trips/schemas/trip.schema.ts
@@ -1,10 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document, Types } from 'mongoose'
+import { Document, ObjectId, Types } from 'mongoose'
 
 export type TripDocument = Trip & Document
 
 @Schema()
 export class Trip {
+	_id: ObjectId
+
 	@Prop({ required: true, type: [String] })
 	participants: string[]
 
@@ -12,4 +14,14 @@ export class Trip {
 	expenses: Types.ObjectId[]
 }
 
-export const TripSchema = SchemaFactory.createForClass(Trip)
+const TripSchema = SchemaFactory.createForClass(Trip)
+
+// Add a virtual property for `id` that converts the `_id` to Base64URL string
+TripSchema.virtual('id').get(function (this: TripDocument) {
+	return Buffer.from(this._id.toString(), 'hex').toString('base64url')
+})
+
+TripSchema.set('toJSON', { virtuals: true })
+TripSchema.set('toObject', { virtuals: true })
+
+export { TripSchema }
