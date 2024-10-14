c283ec22a478ab94b194a22de3a80fc0ea6dff86
kimseungbin <luxorienskim@gmail.com>
2024-10-14 15:44:53 +0900

    Remove: Mock repository in trips service
    Removed the implementation of mock functions in `mockTripsRepository` and replaced it with an empty object.

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -2,16 +2,8 @@ import { Module } from '@nestjs/common'
 import { TripsService } from './trips.service'
 import { TripsController } from './trips.controller'
 
-const mockTripsRepository = {
-	findOne: jest.fn(),
-	create: jest.fn(),
-	save: jest.fn(),
-	update: jest.fn(),
-	remove: jest.fn(),
-}
-
 @Module({
 	controllers: [TripsController],
-	providers: [TripsService, { provide: 'TripsRepository', useValue: mockTripsRepository }],
+	providers: [TripsService, { provide: 'TripsRepository', useValue: {} }],
 })
 export class TripsModule {}
