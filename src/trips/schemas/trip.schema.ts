f12a2d91aeea0e6a4a3b93ec9cca6160fab579e3
kimseungbin <luxorienskim@gmail.com>
2024-10-16 11:09:57 +0900

    Remove: Unused Document import from trip.schema.ts
    The 'Document' import was found to be unused and has been removed in order to clean up the code. This should have no functional impact and is intended to maintain code quality and readability.

--- src/trips/schemas/trip.schema.ts
+++ src/trips/schemas/trip.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document, HydratedDocument, Types } from 'mongoose'
+import { HydratedDocument, Types } from 'mongoose'
 
 export type TripDocument = HydratedDocument<Trip>
 
