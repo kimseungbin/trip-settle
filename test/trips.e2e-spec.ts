f2ec0103eefdcdbf127a7e795398f7bb0e7ad2ee
kimseungbin <luxorienskim@gmail.com>
2024-10-16 17:00:47 +0900

    Test: Add POST /trips E2E test with in-memory MongoDB
    Added end-to-end test for POST /trips endpoint using MongoMemoryServer for an in-memory MongoDB instance. Updated setup to include debugging, and verified that a new trip can be created and found in the database.

--- test/trips.e2e-spec.ts
+++ test/trips.e2e-spec.ts
@@ -3,7 +3,9 @@ import { Test } from '@nestjs/testing'
 import { TripsModule } from '../src/trips/trips.module'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { MongooseModule } from '@nestjs/mongoose'
-import { connection } from 'mongoose'
+import mongoose, { connection, Types } from 'mongoose'
+import { CreateTripDto } from '../src/trips/dto/create-trip.dto'
+import * as request from 'supertest'
 
 describe('Trips', () => {
 	let app: INestApplication
@@ -11,6 +13,8 @@ describe('Trips', () => {
 
 	beforeAll(async () => {
 		mongod = await MongoMemoryServer.create()
+
+		mongoose.set('debug', true)
 
 		const module = await Test.createTestingModule({
 			imports: [TripsModule, MongooseModule.forRoot(mongod.getUri())],
@@ -26,7 +30,25 @@ describe('Trips', () => {
 		await app.close()
 	})
 
-	it.todo('POST /trips')
+	describe('POST /trips', () => {
+		it('should create a new trip', async () => {
+			const createTripDto: CreateTripDto = {
+				participants: ['Alice', 'Bob'],
+			}
+
+			const response = await request(app.getHttpServer()).post('/trips').send(createTripDto).expect(201)
+			const locationHeader = response.header['location']
+			expect(locationHeader).toMatch(/^\/trips\/[a-zA-Z0-9-_]+$/)
+
+			const tripId = locationHeader.split('/').pop()
+			const _id = new Types.ObjectId(Buffer.from(tripId, 'base64url').toString('hex'))
+
+			const tripInDb = await connection.collection('trips').findOne({ _id })
+
+			expect(tripInDb).not.toBeNull()
+			expect(tripInDb.participants).toEqual(createTripDto.participants)
+		})
+	})
 	it.todo('GET /trips/:id')
 	it.todo('PATCH /trips/:id')
 	it.todo('DELETE /trips/:id')
