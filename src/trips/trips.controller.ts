51267174d70a5b262d4f3ee44c5818452204fb0f
kimseungbin <luxorienskim@gmail.com>
2024-10-17 17:53:09 +0900

    Docs: Add Swagger documentation to TripsController
    Enhanced the TripsController by adding Swagger annotations with detailed descriptions for all endpoints. This improves API documentation and helps ensure clarity for developers using the API.

--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -4,23 +4,28 @@ import { CreateTripDto } from './dto/create-trip.dto'
 import { UpdateTripDto } from './dto/update-trip.dto'
 import { FindTripDto } from './dto/find-trip.dto'
 import { Response } from 'express'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
 @Controller('trips')
+@ApiTags('Trips')
 export class TripsController {
 	constructor(private readonly tripsService: TripsService) {}
 
 	@Post()
+	@ApiOperation({ summary: 'Create a new trip', description: 'Create a new trip with the provided participants.' })
 	async create(@Body() createTripDto: CreateTripDto, @Res({ passthrough: true }) res: Response): Promise<void> {
 		const trip = await this.tripsService.create(createTripDto)
 		res.location(`/trips/${trip.id}`)
 	}
 
 	@Get(':id')
+	@ApiOperation({ summary: 'Retrieve a trip', description: 'Gets the details of a trip by its ID.' })
 	async find(@Param('id') id: string): Promise<FindTripDto> {
 		return this.getTripOrFail(id)
 	}
 
 	@Patch(':id')
+	@ApiOperation({ summary: 'Update a trip', description: 'Updates an existing tip with new details.' })
 	@HttpCode(204)
 	async update(
 		@Param('id') id: string,
@@ -35,6 +40,7 @@ export class TripsController {
 	}
 
 	@Delete(':id')
+	@ApiOperation({ summary: 'Delete a trip', description: 'Deletes a trip by its ID.' })
 	@HttpCode(204)
 	async remove(@Param('id') id: string): Promise<void> {
 		await this.getTripOrFail(id)
